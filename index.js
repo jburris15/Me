329d261303285963e9196a67b46fcf62f5ef46d3
jburris15 <jburris15@gmail.com>
2018-09-07 15:30:50 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ window.toggleRight = function() {
     var element = document.getElementById("offcanvas-right");
     element.classList.toggle("hide");
 }
+
+// Select all links with hashes
+// This code allows smooth scrool to anchors
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+/* window.alert("sometext"); */
